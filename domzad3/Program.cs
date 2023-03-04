ae4347ee31e30a0360906eb0813d7e06907486a3
Tatevik8 <tvk25@bk.ru>
2023-03-04 04:15:18 +0300

    Creat array from random real numbers. Write difference between max and min of the numbers.

---
+++ domzad3/Program.cs
@@ -0,0 +1,39 @@
+﻿// **Задача 38:** Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+void GenerateArray(double[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        {
+            array[i] = Convert.ToDouble(new Random().Next(100,1000)) / 100;
+        }
+}
+
+void PrintArray(double[] arr)
+{
+    System.Console.WriteLine("[" + string.Join("; ", arr) + "]");
+}
+
+Console.Write("Введите размер массива:  ");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] myArray = new double[size];
+GenerateArray(myArray);
+PrintArray(myArray);
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int i = 0; i < myArray.Length; i++)
+{
+    if (myArray[i] > max)
+        {
+            max = myArray[i];
+        }
+    if (myArray[i] < min)
+        {
+            min = myArray[i];
+        }
+}
+
+Console.WriteLine($"Максимальное число = {max}. Минимальное число = {min}");
+Console.WriteLine($"Разница между максимальным и минимальным элементов массива = {max - min}");
